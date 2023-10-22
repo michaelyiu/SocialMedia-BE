e0b4b552c4f7d1c25ae710aa4cd1e0c37b84d32b
Michael Yiu <michael.o.yiu@gmail.com>
2023-10-22 16:57:31 -0400

    adjust gitignore for dev config file

---
+++ src/config/keys_dev.ts
@@ -0,0 +1,6 @@
+const keysDev = {
+  MONGO_URI: "mongodb+srv://michaeloyiu:kC6UPJpAgEHQQgGO@cluster0.k2gqwxw.mongodb.net/?retryWrites=true&w=majority",
+  SECRET: "secret"
+};
+
+export default keysDev;
