6a346c5f1dfe13a39e018e2455b3697f0edbff5b
Michael Yiu <michael.o.yiu@gmail.com>
2023-10-22 15:59:29 -0400

    add post model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,6 @@
 import User from './User.js';
 import Profile from './Profile.js'; 
+import Post from './Post.js'; 
 
-const models = { User, Profile };
+const models = { User, Profile, Post };
 export default models;
